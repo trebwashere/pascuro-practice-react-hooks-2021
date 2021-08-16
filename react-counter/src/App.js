614e97d56650935c03e38ca3b97dcec43b987fe2
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-17 00:12:22 +0800

    [STReact][Robert] feat: readded files for react with node_modules

---
+++ react-counter/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MultipleCounter from './MultipleCounter.js';
+
+function App() {
+  return (
+    <MultipleCounter></MultipleCounter>
+  );
+}
+
+export default App;
